fa4f3d6c51f70786c94d72d487027a772d65e7a8
wangwei5 <wangwei641@163.com>
2019-07-17 10:35:39 +0800

    迭代

--- 04-高级特性/02-迭代.py
+++ 04-高级特性/02-迭代.py
@@ -14,3 +14,35 @@ for key in dict:
 # 由于字符串也是可迭代对象，因此，也可以用于for循环
 for ch in 'ABC':
 	print(ch);
+
+# 当我们使用for循环时，只要作用于一个可迭代对象，for循环就可以正常运行，而我们不太关心该
+# 对象究竟是list还是其他数据类型。
+# 如何判断一个对象是可迭代对象呢？方法是通过collections模块的Iterable类型判断：
+from collections import Iterable;
+print('字符串：', isinstance('abc', Iterable));
+print('list：', isinstance([1, 2, 3], Iterable));
+print('数字：', isinstance(123, Iterable));
+
+# 如果要对list实现类似Java那样的下标循环怎么办？
+# Python内置的enumerate函数可以把一个list变成索引-元素对，这样就可以在for循环中同时迭代索引和元素本身
+for i, value in enumerate(['A', 'B', 'C', 'D', 'E']):
+	print(i, value);
+
+
+# 练习
+# 请使用迭代查找一个list中最小和最大值，并返回一个tuple：
+def findMinAndMax(L):
+	minX = L[0];
+	maxX = L[0];
+	if L == []:
+		return (None, None);
+	else:
+		for item in L:
+			if item < minX:
+				minX = item;
+			if item > maxX:
+				maxX = item;
+		return (minX, maxX);
+
+r = findMinAndMax([3, 9, 4, 19, 1, 8]);
+print(r);
