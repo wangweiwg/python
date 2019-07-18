265a72fd9280b22a29b11b9493c9b694be38d7c3
wangweiwg <wangwei641@163.com>
2019-07-18 20:21:25 +0800

    迭代器

---
+++ 04-高级特性/05-迭代器.py
@@ -0,0 +1,6 @@
+# 迭代器
+# 直接作用于for循环的数据类型有以下几种：
+# 一类是集合数据类型，如list、tuple、dict、set、str等
+# 一类是generator，包括生成器和带yield的generator function
+# 这些可以直接作用于for循环的对象称为可迭代对象：Iterable
+# 可以使用isinstance()判断一个对象是否是Iterable对象
