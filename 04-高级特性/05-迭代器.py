d389f52d4fa7b795fa7819f4b73c183e8a5a1f11
wangweiwg <wangwei641@163.com>
2019-07-19 16:05:15 +0800

    迭代器

--- 04-高级特性/05-迭代器.py
+++ 04-高级特性/05-迭代器.py
@@ -10,3 +10,23 @@ print(isinstance({}, Iterable));
 print(isinstance('abc', Iterable));
 print(isinstance((1, 2), Iterable));
 print(isinstance(100, Iterable));
+# 而生成器不但可以作用于for循环，还可以被next()函数不断调用并返回下一个值，直到最后抛出StopIteration
+# 错误表示无法继续返回下一个值了.
+# 可以被next()函数调用并不断返回下一个值的对象称为迭代器Iterator
+# 可以使用isinstance()判断一个对象是否是Iterator对象：
+from collections import Iterator;
+print(isinstance((x for x in range(10))), Iterator); # True
+print(isinstance([], Iterator)); # False
+print(isinstance({}, Iterator)); # False
+print(isinstance('ABC', Iterator)); # False
+# 生成器都是Iterator对象，但list、dict、str虽然是Iterable，却不是Iterator
+# 把list、dict、str/等Iterable变成Iterator可以使用iter()函数；
+isinstance(iter([]), Iterator); # True
+isinstance(iter('ABC'), Iterator); # True
+
+# 为什么list、dict、str等数据类型不是Iterator？
+# 这是因为Python的Iterator对象表示的是一个数据流，Iterator对象可以被next()函数调用并不断返回下一个数据，
+# 知道没有数据时抛出StopIteration错误，可以把这个数据流看做是一个有序序列，但我们却不能提前知道序列的长度，
+# 只能不断通过next()函数实现按需计算下一个数据，所以Iterator的计算是惰性的，只有在需要返回下一个数据时它
+# 才会计算
+# Iterator甚至可以表示一个无限大的数据流，例如全体自然数，而使用list是永远不可能存储全体自然数的。
