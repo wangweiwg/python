8abc8f53eef13ebced5af1626f631ca55c8311bf
wangweiwg <wangwei641@163.com>
2019-08-01 14:18:45 +0800

    debug

--- 09-错误、调试和测试/02-调试.py
+++ 09-错误、调试和测试/02-调试.py
@@ -38,7 +38,7 @@ logging.basicConfig(level=logging.INFO)
 s = '0'
 n = int(s)
 logging.info('n = %d' % n)
-print(10 / n)
+# print(10 / n)
 # logging.info()就可以输出一段文本。运行，发现除了ZeroDivisionError，没有任何信息。怎么回事？
 # 别急，在import logging之后添加一行配置再试试：
 
@@ -51,10 +51,31 @@ print(10 / n)
 
 
 # pdb
-# 第2种方式是启动Python的调试器pdb，让程序以单步方式运行，可以随时查看运行状态，先准备好程序：
+# 第4种方式是启动Python的调试器pdb，让程序以单步方式运行，可以随时查看运行状态，先准备好程序：
+s = '0'
+n = int(s)
+# print(10 / n)
+# 已参数-m pdb启动后，pdb定位到下一步要执行的代码 -> s = '0'
+# 输入命令l来查看代码：
+# 输入n可以单步执行代码
+# 任何时候都可以输入命令 p 变量名 来查看变量
+# 输入命令 q 结束调试，退出程序
+
+
+# 这种通过pdb在命令调试的方法理论上是万能的，但实在是太麻烦了，如果有一千行代码，要运行到第999行得敲多少命令
+# 啊，还好，我们还有另一种调试方法
 # 
-
-
+# 
+# pdb.set_trace()
+# 这个方法也是用pdb，但是不需要单步执行，我们只需要import pdb，然后，在可能出错的地方放一个pdb.set_trace(),
+# 就可以设置一个断点：
+import pdb
+s1 = '0'
+n1 = int(s1)
+pdb.set_trace()
+print(10 / n)
+# 运行代码，程序会自动在pdb.set_trace()暂停并进入pdb调试环境，
+# 可以用命令p查看变量，或者用命令c继续运行
 
 
 
