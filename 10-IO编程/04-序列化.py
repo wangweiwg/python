122df0487b9d1a44a0cf7a0049758a7557471d19
wangweiwg <wangwei641@163.com>
2019-08-05 14:35:37 +0800

    序列化

--- 10-IO编程/04-序列化.py
+++ 10-IO编程/04-序列化.py
@@ -46,14 +46,57 @@ print(c) # 变量的内容又回来了
 # Python内置的json模块提供了非常完善的Python对象到JSON格式的转换
 # 先看看如何把Python对象变成一个JSON：
 import json
+d2 = dict(name = 'Tom', age = 30, score = 98)
+str2 = json.dumps(d2)
+print(str2)
+# dumps()方法返回一个str，内容就是标准的JSON。类似的，dumo()方法可以直接把JSON写入一个file-like Object.
+# 要把JSON反序列化为Python对象，用loads()或者对应的load()方法，前者把JSON的字符串反序列化，后者从file-like
+# Object中读取字符串并反序列化：
+json_str = '{"name": "Tom", "age": 30, "score": 98}'
+d3 = json.loads(json_str)
+print(d3)
+# 由于JSON标准规定JSON编码是UTF-8，所以我们总是能正确地在Python str与JSON的字符串之间转换
 
 
+# JSON进阶
+# Python的dict对象可以直接序列化为JSON的{}, 不过，很多时候，我们喜欢用class表示对象，比如定义Student类，
+# 然后序列化：
+import json
+class Student(object):
+	def __init__(self, name, age, score):
+		self.name = name
+		self.age = age
+		self.score = score
+
+s = Student('Bob', 20, 88)
+# print(json.dumps(s)) # 得到一个TypeError
+# 错误的原因是Student对象不是一个可序列化为JSON的对象
+# 如果连class的实例对象都无法序列化为JSON，这肯定不合理
+# 别急，我们仔细看看dumps()方法的参数列表，可以发现，除了第一个必须的obj参数外，dumps()方法还提供一大堆的可选参数
+# 这些可选参数就是让我们定制JSON序列化，前面的代码之所以无法把Student类实例序列化JSON，是因为默认情况下，
+# dumps()方法不知道如何将Student实例变为一个JSON的{}对象
+# 可选参数default就是把任意一个对象变成一个可序列化为JSON的对象，我们只需要为Student专门写一个转换函数，
+# 再把函数传进去即可：
+def StudentToDict(std):
+	return {
+		'name': std.name,
+		'age': std.age,
+		'score': std.score
+	}
+# 这样，Student实例首先被StudentToDict()函数转换为dict，然后再被顺利序列化为JSON
+json_str2 = json.dumps(s, default = StudentToDict)
+print(json_str2)
+# 不过，下次如果遇到一个Teacher类的实例，照样无法序列化为JSON。我们可以偷个懒，把任意class的实例变为dict：
+json.dumps(s, default = lambda obj: obj.__dict__)
+# 因为通常class的实例都有一个__dict__属性，它就是一个dict，用来存储实例变量。也有少数例外，比如定义了__slots__的class
 
 
+# 同样的道理，如果我们要把JSON反序列化为一个Student对象实例，loads()方法首先转换出一个dict对象，然后，我们传入的
+# object_hook函数负责把dict转换为Student实例：
+def dict2student(d):
+	return Student(d['name'], d['age'], d['score'])
 
-
-
-
-
+json_student = json.loads(json_str2, object_hook = dict2student)
+print(json_student)
 
 
