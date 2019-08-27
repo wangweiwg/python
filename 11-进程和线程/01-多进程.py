6ef8d0751bb8a95d5666efd19cb3649ac429767e
wangweiwg <wangwei641@163.com>
2019-08-27 15:25:47 +0800

    多线程

--- 11-进程和线程/01-多进程.py
+++ 11-进程和线程/01-多进程.py
@@ -68,10 +68,25 @@ if __name__ == '__main__':
 
 # Poll
 # 如果要启动大量的子进程，可以用进程池的方式批量创建子进程：
-from mulitiprocessing import Poll
+from multiprocessing import Poll
 import os, time, random
- 
+
+def long_time_task(name):
+	print('Run task %s (%s)....' % (name, os.getpid()))
+	start = time.time()
+	time.sleep(random.random() * 3)
+	end = time.time()
+	print('Task %s runs %0.2f seconds.' % (name, end - start))
 
 
+if __name__ == '___main__':
+	print('Parent process %s' % os.getpid())
+	p = Pool(4)
+	for i  in range(5):
+		p.apply_async(long_time_task, args=(i, ))
+	print('Waiting for all subprocesses done...')
+	p.close()
+	p.join()
+	print('All subprocesses done.')
 
-
+# 费解中。。。
